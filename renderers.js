45639c0368fb4b00767550d9098963fb28a463f3
Pierrick Varin <pierrick.varin@glitchstudio.com>
2015-01-22 02:25:06 +0100

    Simpler getRandomVectorInHemisphere method for pathtracer renderer. It's very biased (non uniform) but it'll help with debugging the renderer

--- renderers.js
+++ renderers.js
@@ -5,32 +5,15 @@ Renderer.pathtracer = function() {
 	this.seedVector = null;
 	this.traceDepth = 6;
 
-	this.vecOrtho = function(v) {
-	    if( Math.abs(v.elements[0]) > Math.abs(v.elements[2]) ) {
-	    	return $V([-v.elements[1], v.elements[0], 0.0]);
-	    } else {
-	    	return $V([0.0, -v.elements[2], v.elements[1]]);
-	    }
-	}
-
-	this.rand2n = function() {
-		this.seedVector = this.seedVector.add($V([-1, 1]));
-	    return $V([ Math.sin(globals.currentScreenX * Math.random()) % 1, Math.cos(globals.currentScreenY * Math.random()) % 1])
-	}
-
 	this.getRandomVectorInHemisphere = function(normal) {
-		var dir = normal.toUnitVector();;
-		var o1  = this.vecOrtho(dir).toUnitVector();
-		var o2  = dir.cross(o1).toUnitVector();;
-		var r = this.rand2n();
-		r.elements[0] = r.elements[0] * 2 * Math.PI;
+		var dir = normal.toUnitVector();
+		var rndDir = $V([-1 + Math.random() * 2, -1 + Math.random() * 2, -1 + Math.random() * 2]);
 
-		var oneminus = Math.sqrt(1.0 - r.elements[1] * r.elements[1]);
-		var p1 = o1.multiply(Math.cos(r.elements[0]) * oneminus);
-		var p2 = o2.multiply(Math.sin(r.elements[0]) * oneminus);
-		var p3 = dir.multiply(r.elements[1]);
+		if( rndDir.dot(dir) < 0 ) {
+			rndDir = rndDir.multiply(-1);
+		}
 
-		return p1.add(p2).add(p3);
+		return rndDir;
 	}
 
 	this.getColor = function(intersection) {
