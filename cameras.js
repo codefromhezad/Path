199d24e212baf9c9ffd7479c0d9e25f0ce2b42d9
Pierrick Varin <pierrick.varin@glitchstudio.com>
2015-01-25 14:00:31 +0100

    Added basic perspective projection camaera (camera direction is yet to be implemented though. Only looking forward for now

--- cameras.js
+++ cameras.js
@@ -30,3 +30,36 @@ Camera.orthographic = function(opts) {
 		return new Ray(p, d);
 	}
 }
+
+
+Camera.perspective = function(opts) {
+	this.defaults = {
+		position: $V([0, 0, -2]),
+		direction: $V([0, 0, 1]),
+		viewportWidth: 20,
+		viewportDistance: 10
+	}
+	this.options = extend(this.defaults, opts);
+
+	this.position = this.options.position;
+	this.viewportWidth = this.options.viewportWidth;
+	this.viewportHeight = this.options.viewportWidth * Engine.aspectRatio;
+	this.viewportDistance =this.options.viewportDistance;
+
+	this.position.elements[2] -= this.viewportDistance;
+
+	this.getRay = function(x, y) {
+		var xLerp = x / Engine.width;
+		var yLerp = y / Engine.height;
+
+		var pArr = [];
+		pArr[0] = -this.viewportWidth * 0.5 + this.viewportWidth * xLerp;
+		pArr[1] = this.viewportHeight * 0.5 - this.viewportHeight * yLerp;
+		pArr[2] = -this.viewportDistance;
+
+		var d = $V([pArr[0], pArr[1], this.viewportDistance]).toUnitVector();
+		var p = this.position;
+
+		return new Ray(p, d);
+	}
+}
